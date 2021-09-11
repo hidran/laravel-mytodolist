f4ff98d68455b1eb8d14b20ef5317ee9547d1b7a
Hidran Arias <hidran@gmail.com>
2021-09-11 18:00:16 +0200

    adds laravel ide-helper

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Models\User;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +16,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('user', function(){
+   
+  return User::where('email', 'hidran2@gmail.com')->get();
 });
